7d6e34a3ff0d176665d38e060b7f9bb1f6246403
AlexDrozd <shuradrozd@mail.ru>
2017-11-10 19:01:15 +0300

    add new logic with output correct values

---
+++ old_logic.js
@@ -0,0 +1,78 @@
+// fs.readdir(curDir, function (err, files) {
+//     if (err) {
+//         return console.error(err);
+//     }
+//
+//     for (let i = 0; i < files.length; i++) {
+//         let country = files[i].slice(0, 2);
+//         console.log(`import ${country} from ${curDir}${country}`);
+//         //const txtFile = new JFile(`./src/js/${files[i]}`);
+//         //let result = txtFile.grep('components.container.Artist.User.notFound', true) ;
+//         //console.log(`${files[i]} = ${result[0].i + 1}`);
+//
+//     }
+// });
+
+
+
+
+// const ruArr = [];
+// const enArr = [];
+// addPropToArr(en, enArr);
+// addPropToArr(ru, ruArr);
+//
+//
+//
+// function addPropToArr(obj, arr)
+// {
+//     for (let key in obj) {
+//         arr.push(key);
+//     }
+// }
+// console.log(`RU DIC ${ruArr.length}`);
+// console.log(`EN DIC ${enArr.length}`);
+//
+// function checkProps(arr1, arr2) {
+//     let str = ``;
+//     if (arr1.length >= arr2.length) {
+//         for (let i = 0; i < arr1.length; i++) {
+//
+//             for (let j = 0; j < arr2.length; j++) {
+//                 if (arr1[i] == arr2[j]) {
+//                     arr1.splice(i, 1);
+//                 }
+//             }
+//         }
+//         if (arr1.length) {
+//             let properties = arr1.reduce((prev, cur) => `${prev} \n ${cur}`);
+//             str = `In dictionary EN exist property(ies): \n ${properties} \n that doesn't exist in RU dictionary`;
+//         } else {
+//             str = `All properties are the same`;
+//         }
+//         return str;
+//     } else {
+//         for (let i = 0; i < arr2.length; i++) {
+//
+//             for (let j = 0; j < arr1.length; j++) {
+//                 if (arr2[i] == arr1[j]) {
+//                     arr2.splice(i, 1);
+//                 }
+//             }
+//         }
+//
+//         if (arr2.length) {
+//             let properties = arr2.reduce((prev, cur) => `${prev} \n ${cur}`);
+//             str = `In dictionary RU exist property(ies): \n ${properties} \n that doesn't exist in EN dictionary`;
+//         } else {
+//             str = `All properties are the same`;
+//         }
+//         return str;
+//     }
+// }
+//
+// console.log(checkProps(enArr, ruArr));
+
+
+
+
+
--- search.js
+++ search.js
@@ -5,7 +5,9 @@ const fs = require("fs");
 const JFile = require('jfile');
 const curDir = `./src/js/`;
 
-setTimeout(() =>{
+
+setTimeout( () => {
+
     fs.readdir(curDir, function (err, files) {
         if (err) {
             return console.error(err);
@@ -13,38 +15,65 @@ setTimeout(() =>{
 
         for (let i = 0; i < files.length; i++) {
             let country = files[i].slice(0, 2);
+            //obj[`${country}Arr`] = [];
             console.log(`import ${country} from ${curDir}${country}`);
 
-            //const txtFile = new JFile(`./src/js/${files[i]}`);
-            //let result = txtFile.grep('components.container.Artist.User.notFound', true) ;
-            //console.log(`${files[i]} = ${result[0].i + 1}`);
-
-        }
-    });
+       }
+    //console.log(obj);
+});
 }, 1000);
-// fs.readdir(curDir, function (err, files) {
-//     if (err) {
-//         return console.error(err);
-//     }
-//
-//     for (let i = 0; i < files.length; i++) {
-//         let country = files[i].slice(0, 2);
-//         console.log(`import ${country} from ${curDir}${country}`);
-//         //const txtFile = new JFile(`./src/js/${files[i]}`);
-//         //let result = txtFile.grep('components.container.Artist.User.notFound', true) ;
-//         //console.log(`${files[i]} = ${result[0].i + 1}`);
-//
-//     }
-// });
 
 
-//    import ru from './src/js/ru';
- //   import en from './src/js/en';
-setTimeout(()=>{
+
+
+setTimeout(() => {
+
     const ruArr = [];
     const enArr = [];
+
+
     addPropToArr(en, enArr);
     addPropToArr(ru, ruArr);
+
+    function savePropPos(arr, fileName) {
+        let txtFile = new JFile(`${curDir}${fileName}`);
+        let result;
+        let arrPos = [];
+        for (let i = 0; i < arr.length; i++) {
+            result = txtFile.grep(arr[i], true) ;
+
+            arrPos.push(result[0].i + 1);
+        }
+        return arrPos;
+    }
+
+    let ruArrPos = savePropPos(ruArr, `ru.js`);
+    let enArrPos = savePropPos(enArr, `en.js`);
+
+
+    function checkSync(arrPos1, arrPos2) {
+        let strPos = ``;
+        for (let i = 0; i < arrPos1.length; i++) {
+
+            if (arrPos1[i] !== arrPos2[i]) {
+                strPos += `First async position is : ${arrPos2[i]}, but should be ${arrPos1[i]}`;
+                break;
+            }
+
+
+
+
+        }
+        if (strPos) {
+          console.log(strPos);
+        } else {
+          console.log(`Strings in files are sync!!!`);
+        }
+    }
+
+    if (ruArr.length == enArr.length) {
+        checkSync(enArrPos, ruArrPos);
+    }
 
 
 
@@ -58,106 +87,96 @@ setTimeout(()=>{
     console.log(`EN DIC ${enArr.length}`);
 
     function checkProps(arr1, arr2) {
-        let str = ``;
+        let outStr = ``;
+        let checkArr1 = arr1;
+        let checkArr2 = arr2;
+
         if (arr1.length >= arr2.length) {
             for (let i = 0; i < arr1.length; i++) {
 
                 for (let j = 0; j < arr2.length; j++) {
                     if (arr1[i] == arr2[j]) {
                         arr1.splice(i, 1);
+                        i--;
                     }
                 }
             }
             if (arr1.length) {
-                let properties = arr1.reduce((prev, cur) => `${prev} \n ${cur}`);
-                console.log(properties);
-                let txtFile = new JFile(`./src/js/ru.js`);
-                let result = txtFile.grep(`${properties}`, true) ;
-               console.log(`ru.js = ${result[0].i + 1}`);
-                str = `In dictionary EN exist property(ies): \n ${properties} \n that doesn't exist in RU dictionary on position ${result[0].i + 1}`;
+
+                let positions = savePropPos(arr1, `en.js`);
+                //let properties = arr1.reduce((prev, cur) => `${prev} \n ${cur}`);
+                let output = [];
+                let str = ``;
+                for (let i = 0; i < arr1.length; i++) {
+                    output.push(
+                        {
+                            property: arr1[i],
+                            position: positions[i]
+                        }
+                    );
+                }
+
+                for (let i = 0, j = 1; i < output.length; i++, j++) {
+                    str += `${j}) ${output[i]['property']} on position: ${output[i]['position']} \n`;
+                }
+
+                outStr = `In dictionary EN exist property(ies):\r\n${str}that doesn't exist in RU dictionary`;
+
             } else {
-                str = `All properties are the same`;
+                //outStr = `All properties are the same`;
+
+                for (let i = 0; i < checkArr1.length; i++) {
+                    console.log(i);
+                    if (checkArr1[i] != checkArr2[i]) {
+                        outStr = `Property ${checkArr1[i]} in dictionary EN doesn't equal property in RU dictionary`;
+                        break;
+                    }
+                }
+                console.log('test');
             }
-            return str;
+
+            return outStr;
+
         } else {
             for (let i = 0; i < arr2.length; i++) {
 
                 for (let j = 0; j < arr1.length; j++) {
                     if (arr2[i] == arr1[j]) {
                         arr2.splice(i, 1);
+                        i--;
                     }
                 }
             }
 
             if (arr2.length) {
-                let properties = arr2.reduce((prev, cur) => `${prev} \n ${cur}`);
-                str = `In dictionary RU exist property(ies): \n ${properties} \n that doesn't exist in EN dictionary on position ${result[0].i + 1}`;
+
+                let positions = savePropPos(arr2, `ru.js`);
+                //let properties = arr1.reduce((prev, cur) => `${prev} \n ${cur}`);
+                let output = [];
+                let str = ``;
+                for (let i = 0; i < arr2.length; i++) {
+                    output.push(
+                        {
+                            property: arr2[i],
+                            position: positions[i]
+                        }
+                    );
+                }
+
+                for (let i = 0, j = 1; i < output.length; i++, j++) {
+                    str += `${j}) ${output[i]['property']} on position: ${output[i]['position']} \n`;
+                }
+
+                outStr = `In dictionary RU exist property(ies):\r\n${str}that doesn't exist in EN dictionary`;
+
             } else {
-                str = `All properties are the same`;
+                outStr = `All properties are the same`;
             }
-            return str;
+
+            return outStr;
         }
     }
 
     console.log(checkProps(enArr, ruArr));
-}, 5000);
-    // const ruArr = [];
-    // const enArr = [];
-    // addPropToArr(en, enArr);
-    // addPropToArr(ru, ruArr);
-    //
-    //
-    //
-    // function addPropToArr(obj, arr)
-    // {
-    //     for (let key in obj) {
-    //         arr.push(key);
-    //     }
-    // }
-    // console.log(`RU DIC ${ruArr.length}`);
-    // console.log(`EN DIC ${enArr.length}`);
-    //
-    // function checkProps(arr1, arr2) {
-    //     let str = ``;
-    //     if (arr1.length >= arr2.length) {
-    //         for (let i = 0; i < arr1.length; i++) {
-    //
-    //             for (let j = 0; j < arr2.length; j++) {
-    //                 if (arr1[i] == arr2[j]) {
-    //                     arr1.splice(i, 1);
-    //                 }
-    //             }
-    //         }
-    //         if (arr1.length) {
-    //             let properties = arr1.reduce((prev, cur) => `${prev} \n ${cur}`);
-    //             str = `In dictionary EN exist property(ies): \n ${properties} \n that doesn't exist in RU dictionary`;
-    //         } else {
-    //             str = `All properties are the same`;
-    //         }
-    //         return str;
-    //     } else {
-    //         for (let i = 0; i < arr2.length; i++) {
-    //
-    //             for (let j = 0; j < arr1.length; j++) {
-    //                 if (arr2[i] == arr1[j]) {
-    //                     arr2.splice(i, 1);
-    //                 }
-    //             }
-    //         }
-    //
-    //         if (arr2.length) {
-    //             let properties = arr2.reduce((prev, cur) => `${prev} \n ${cur}`);
-    //             str = `In dictionary RU exist property(ies): \n ${properties} \n that doesn't exist in EN dictionary`;
-    //         } else {
-    //             str = `All properties are the same`;
-    //         }
-    //         return str;
-    //     }
-    // }
-    //
-    // console.log(checkProps(enArr, ruArr));
-
-
-
-
-
+}, 2000);
+    
